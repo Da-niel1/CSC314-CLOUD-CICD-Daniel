094c121d176cbef654887d0c2332a233b591629f
Da-niel1 <demelogu60@gmail.com>
2024-05-28 20:36:55 +0100

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
-app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME: DANIEL UGOCHUKWU EMELOGU. MATRIC. NO: VUG/CSC/21/6263'));
+app.use(express.static("MAIN"));
+//app.get('/', (req, res) => res.send('THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS. THIS PAGE INDICATES SUCCESS. NAME: DANIEL UGOCHUKWU EMELOGU. MATRIC. NO: VUG/CSC/21/6263'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
